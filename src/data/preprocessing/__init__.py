a3e5be4a63ce8e424ce5ce3b2ee5cbb429b5eeb8
Garnet Owl <james544wanjiku@gmail.com>
2025-02-22 22:36:04 +0300

    Refactor preprocessing script for backward compatibility and add new init file

---
+++ src/data/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+"""
+Preprocessing package for Taura project.
+"""
+
+from src.data.preprocessing.utils import clean_text, preprocess_data, split_data, save_splits
+
+__all__ = ["clean_text", "preprocess_data", "split_data", "save_splits"]
